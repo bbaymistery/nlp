12b9b3b9939198d014ccd9ade26de91141c7a0eb
bbaymistery <elgun.ezmemmedov@mail.ru>
2025-05-18 16:25:50 +0400

    S

---
+++ 2_NLP_Varsayimlari/18_Seçenek_Özgürlüktür_Seçeneksizlik_iseKölelik/note.js
@@ -0,0 +1,69 @@
+/**
+Ozgurluk cofazla seceneye sahip olmakla mumkundur 
+Kolelik ise tek bir secenege bagli kalmakla mumkundur
+Ne demek bu ? 
+Secenek demek aslinda ozgutluk demektir
+Tek bir secenek secenek degildir Bunun adi mecburyetdir 
+Tercih ederseniz yada etmezsiniz sadece tek bir seceneginiz vardir
+Iki tane secenek bir celiski olusdurur cogu durumda
+Kafaniz karisir hangisini secsem diye
+3 ve yada daha fazla secenek gercek bir ozgurluk alani acar bize  
+Bu su anlama gelir: 
+ Eger bizim mutlu olmak,kendimizi basarili hiss etmek,kendimize guven duymak konusunda tek bi
+  secenegimiz varsa .Mesela Mutlu olmak icin sadece cok param olmali secenege indirgediysem 
+   Bunun disinda kalan hic bir secenek beni mutlu etmiycekdir
+  Zaten bu secenekleri ben gormuyecegim bile Cunki afam sadece para varsa mutluluk inanci var
+  Iki tane secenegim varsa beni mutlu edicek 
+  Bunlarda aslinda beni tatmin etmicek cunki
+  Bunlarin icinde bir celiskiye dusucem
+  Ancak 3 ve yadaha fazla secenek varsa bana kendimi iyi hiss etdire bilicek ,hemen her durumda 
+   kullanam bilecegim Tipki bir usta cilingirin alet cantasinda kullandigi arac gerecler gibi
+    bana ozgurluk alani taniycak olan secenekler seklinde ozgurlukle buluscam
+
+    Kendini iyi hiss etmek icin cok fazla secenegin olmali 
+    Kendini mutlu hiss etmek icin cok fazla secenegin olmali 
+    Tek bir secenekle dusunen zihnini ,gelisdirerek,genisleterek,dusuncelerini cogaltarak 
+     farkli farkli dusunceleri urete bilecek hale getirmelisiz
+     Bazi basit pratik uygulamalar yapa bilirsiniz 
+     Mesela bir toplu iyne ile neler yapa bileceginize dair liste hazrlamaya calisin
+    1. Balon patlatmak  
+    2. Meyve soymak (narenciye kabuğunu delmek gibi küçük işler)  
+    3. Ete batan kıymığı çıkarmak  
+    4. Not yapıştırmak (kağıdı mantar panoya tutturmak)  
+    5. Tablo asmak  
+    6. Takı yapmak (küpe, broş gibi tasarımlarda iğne kullanmak)  
+    7. Şarj kablosu sarkmasın diye sabitlemek  
+    8. Perdeyi toplamak (iğneyle geçici tutturmak)  
+    9. Telefon hoparlöründeki tozu temizlemek  
+    10. Panoya çoklu şekilde kağıt sabitleyerek yazı oluşturmak
+
+     Burdaki zenginlesdirmeni kendi dusunce yapimizida yaparakda yapa biliriz
+     Hyatda karsimiza cika bilecek sorunlarla hangi seceneklere acaba acigiz 
+     Bu secenekler neler acaba
+     Tipki bir toplu igne ile yapa bilecegimiz seylerin seceneklerini cogaltmak gibi
+     Yasama mutlu ola bilmem icin neler yapa bilirim  diye dusunursek Secenekleri cogalta biliriz
+     Sonraki adimda ise su anda beni hayatimda mutlu edecek seyler diye bir liste yapin 
+     Bakin bakalim listede neler var
+
+
+      ✅ Memnun Olduğum Şeyler Listesi (Gerçek Memnuniyet Odağında)
+
+      1. Sağlıklıyım.  
+      2. Kendime güveniyorum.  
+      3. Kendimi başarılı hissediyorum.  
+      4. Spora gidiyorum.  
+      5. Ortamlara çok çabuk uyum sağlıyorum.  
+      6. İnsanlara pozitif bir enerji veriyorum.  
+      7. Kararlarımın sorumluluğunu alabiliyorum.  
+      8. Mesleğimden memnunum.  
+      9. Yardımsever bir insanım.  
+      10. İnançlıyım.  
+      11. İnsanlara psikolojik destek vermek ve onları yönlendirmekten memnunum.
+
+
+     Sahib oldugum yetenekler ,beceriler listesi
+
+     Yazn bakalim odaklandiginizda neleri goreceksiniz kucuk yada buyuk fark etmez bunlarida yazin
+     
+     Seceneklerimiz yasam kalitemizi yansitir 
+ */
---
+++ 2_NLP_Varsayimlari/19._Değişmek_ya_da_Acı_Çekmek_Bir_Seçimdi/note.js
@@ -0,0 +1,29 @@
+/**
+Degismek yada aci cekmek
+Acba bu bir secim ola bilirmi ?
+Evet coogu durumda bu bir secimdir heryerde degil tabii
+Ozellikle genelleme yapmaktan kacinmaliyiz.Nitcheninde soyledigi gibi hatda tehlikelidir
+Butun genellemeler tehlikelidir desek bile yine genelleme yapariz ve buda bir genellemedir
+
+Ishde bu nedenle insanlar kendi hayatlarini kendileri sekillendirir
+Her davranisin altinda bir olumlu niyet vardir ve degismek yada aci cekmek bir secimdir cogunlukla
+
+Degismek bir seyleri fark edib ise yaramadigini anladiginiz seyleri artik yapmamaya yada daha iyilesdirmeeye
+ yada onlara bir alternatif bulmaya hizmet eder
+
+Ama aci cekmek onlari kabullenmektir,onlara boyun egmektir ve bunun teknik adi kurban roluna girmektir
+Kurban roulne girerse bir insan,aci cekmenin hayatinin,yasaminin,kaderinin,bir parcasi olarak gorur.
+ Ve bu yasam boyu devam eden bi hayat hikayesi olur Ve o zaman her sey bu hikayeyi dogruluyacaktir
+  Karsiniza cikan insanlar bunu dogrular Hayat hikayenize bakdiginizda ben boyleyim cunki boyle bi hikayem var dersiniz
+
+    ve isin kotu tarafi bundan sonrada bu hikayeyi dogrulamak icin hayatinizi aynen hikayenizde oldugu gibi
+     secersiniz.Bir duzeyde korkularimizi gerceklesidiriz,Bir duzeyde korkularimiz hayatimizin bir parcasi olur
+     Degismek yada aci cekmek bir secimdir
+
+    Kurban rolunden uzaklasib hayatda neye ihtiyac duydugumuzu ve hangi kaynaklarla elde ede bilecegimize odaklanmaliyiz
+
+
+
+
+
+*/
