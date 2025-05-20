983853cc0d863bac753233ce62651faa9c3910e9
bbaymistery <elgun.ezmemmedov@mail.ru>
2025-05-21 00:30:10 +0400

    s

---
+++ 2_NLP_Varsayimlari/20_HerDavranışınBirYapısıVardır/note.js
@@ -0,0 +1,44 @@
+/*
+Güneş sadece gökyüzünü aydınlatır, ama sen girdiğin her yeri... hatta kalpleri bile.
+
+
+Haytin bir yapisi ,skeleti,teknik bir yapisi vardir
+Davranislar durub dururken karsimiza cikan Bir takim yansimalar yada sonuclar degildir
+Davranislarimizin icinde bir hikaye vardir 
+Egitimin basinda ne demisdik 
+Dusunceler duygulara ,duygular davranislara ,davranislarda sonuclara yol acar
+Davranuis dedigimiz seyin geri planinda aslinda dusunce ve duygularimizin bizi harekete gecirdigi
+ yonlendirdigi bir gizli strateji vardir
+Davranislarin bir sirasi bir isleyis sekli ve gercektende bir kodu vardir
+Mutsuz ola bilmemiz icin ,pek cok geri planda isleyen programi kullanmamiz gerektir
+Mesela mutsuz ola bilmek icin oncelikle insan bedenini mutsuz biri gibi kullanmasi gerek.Bunu ogrenmesi gerek
+Omuzlarin dusuk olmasi,ses tonlarin dusuk seviyede cikmasi,gozlerinin soluk ,cansiz,bitik hal almasi,
+ butun vucudundaki enerjinin adeta emilib sokulub gitmesi,Kendi kendine zihninde uretdigi zihnindeki dusuncelerinin
+  mutsuzlugu destekleyecek dusunceler olmasi
+ Kendine sordugu sorular Butun zihninden gecen goruntuler
+ Cunki bunu anlatirken bile ne kadar cansiz gozukdugumu fark etdinizmi .bU KOLAY BI SEY DEGILKI BUNU YAPMANIZ LAZIM
+  Depresyona girmek kolay bi sey degilki Oyle depresyona gitriyorum diyib depresyona girilmezki 
+  Depresyonun bir yapisi vardir Ve insanlar bu yapiya uygun bir sekilde davranislari ve duygulari olusdurur
+  Her davranisin bir yapisi bir stratejisi vardir.Kendini tanimak demek neyi neden nasil nerede ne sekilde yapiyorum 
+   sorularina yanit vermek  demek 
+   Yani ben bunu neden yapiyorum.Ofkeliyim Ofke hangi amca hizmet ediyor.Bu ofke kime.Ben gercekte kimden nefret ediyorum
+   Bu sorular bize davranisi niye neden yapdigimizi anlatir
+   dVARANISLARIMIZI ANALIZ EDEREK SORGULAMAK YONLENDIRMEK NLP AMACLARINDAN BIRIDIR
+   Nlp bir ogrenme modelidir
+   Su ana kadar butun davranislarimizi biz ogrendik Bunlari bir aliskanlik haline getirdik
+  Nlp nin Neuro liguistic programlama kisminda anlatdigi seyi yapdik
+  Yani aliskanliklara dobngulere ve kaliplara donusdurduk
+  Faakat guzel olan haber shudurki butun davranislar paternleride degisdirile bilir
+  Evet nlp varsayimlari icin uzun bir bolum ayirdik 
+  Genel olarak hayatda karsilasdiginiz zorluklar celiskiler oldugunda Bu nlp varsayimlarina bi bakarsaniz Hayatinizda onemli sekilde 
+   yon vermis olursunuz
+   Burdaki varsayimlari soylede dusune bilirsiniz
+   Hayatda bi aksilikle ters bi durumla ,sizi uzecek mutsuz edecek bi seyle karsilasdiginda
+   donun ve bu bolume bir bakin
+   Bu bolumde gordugunuz bazi ilkeleri ihlal etmis ola bilirsiniz
+   Belkide beden zihin et ve tirnak gibidir ilkesine aykiri davrandiniz
+   Kendinizi hareketsiz cansiz enerjisiz halde buldunuz ve sonrada ruh haliniz etkilendi
+   Belkide basarisizlik yokdur sadece sonuc vardir ilkesine ihtiyaciniz vardir Cunki kendinizi bir 
+    basarizi olarak etiketlediniz
+    Yasamda karsilasidginiz olaylar her ne olursa olsun Lutfen bu bolumdeki ilkerelere bir bakin
+*/
